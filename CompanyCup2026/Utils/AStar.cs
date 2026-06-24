06d909221468a9306dd07b1f7a30c9210a5478cb
Dewald Swanepoel <dewald.swanepoel@gmail.com>
2026-06-24 19:33:01 +0200

    Added AStar algorithm

---
+++ CompanyCup2026/Utils/AStar.cs
@@ -0,0 +1,106 @@
+﻿namespace CompanyCup2026.Utils
+{
+    public static class AStar
+    {
+        public static IReadOnlyList<TNode>? FindPath<TNode>(
+            IGraph<TNode> graph,
+            TNode start,
+            TNode goal)
+            where TNode : notnull
+        {
+            var openNodes = new PriorityQueue<TNode, double>();
+
+            var closedNodes = new HashSet<TNode>();
+
+            var previousNode = new Dictionary<TNode, TNode>();
+
+            var costFromStart = new Dictionary<TNode, double>
+            {
+                [start] = 0
+            };
+
+            openNodes.Enqueue(
+                start,
+                graph.EstimateCost(start, goal));
+
+            while (openNodes.Count > 0)
+            {
+                var currentNode = openNodes.Dequeue();
+
+                // Ignore stale queue entries
+                if (!closedNodes.Add(currentNode))
+                {
+                    continue;
+                }
+
+                if (EqualityComparer<TNode>.Default.Equals(currentNode, goal))
+                {
+                    return ReconstructPath(
+                        previousNode,
+                        currentNode);
+                }
+
+                foreach (var neighbour in graph.GetNeighbours(currentNode))
+                {
+                    if (closedNodes.Contains(neighbour))
+                    {
+                        continue;
+                    }
+
+                    var costToReachNeighbour =
+                        costFromStart[currentNode] +
+                        graph.GetCost(currentNode, neighbour);
+
+                    var knownCostToReachNeighbour =
+                        costFromStart.GetValueOrDefault(
+                            neighbour,
+                            double.PositiveInfinity);
+
+                    if (costToReachNeighbour >= knownCostToReachNeighbour)
+                    {
+                        continue;
+                    }
+
+                    previousNode[neighbour] = currentNode;
+
+                    costFromStart[neighbour] =
+                        costToReachNeighbour;
+
+                    var estimatedTotalPathCost =
+                        costToReachNeighbour +
+                        graph.EstimateCost(neighbour, goal);
+
+                    openNodes.Enqueue(
+                        neighbour,
+                        estimatedTotalPathCost);
+                }
+            }
+
+            return null;
+        }
+
+        private static IReadOnlyList<TNode> ReconstructPath<TNode>(
+            Dictionary<TNode, TNode> previousNode,
+            TNode goal)
+            where TNode : notnull
+        {
+            var path = new List<TNode>();
+
+            var currentNode = goal;
+
+            path.Add(currentNode);
+
+            while (previousNode.TryGetValue(
+                       currentNode,
+                       out var parentNode))
+            {
+                currentNode = parentNode;
+                path.Add(currentNode);
+            }
+
+            path.Reverse();
+
+            return path;
+        }
+    }
+}
---
+++ CompanyCup2026/Utils/IGraph.cs
@@ -0,0 +1,11 @@
+﻿namespace CompanyCup2026.Utils
+{
+    public interface IGraph<TNode>
+    {
+        IEnumerable<TNode> GetNeighbours(TNode node);
+
+        double GetCost(TNode from, TNode to);
+
+        double Heuristic(TNode from, TNode goal);
+    }
+}
